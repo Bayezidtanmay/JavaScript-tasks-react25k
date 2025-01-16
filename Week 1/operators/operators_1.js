247ae808dc0d269a7fd7e3667cb091fb13e42c7a
Bayezidtanmay <bayzidtonmoy123@gmail.com>
2025-01-16 14:57:13 +0200

    end of lession commit

---
+++ Week 1/operators/operators_1.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+
+let add = a + b;
+console.log("The addition of a and b is", add);
+
+let sub = b - a;
+console.log(sub);
+
+let multy = a * b;
+console.log(multy);
---
+++ Week 1/variables/variables_1.js
@@ -0,0 +1,3 @@
+let count = 0;
+plo = count + 1;
+console.log(plo);
