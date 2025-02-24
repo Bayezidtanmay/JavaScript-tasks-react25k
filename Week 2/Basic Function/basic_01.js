cd2c5a36b4cf9553863aa2f9a6bd3bdf5dc6e242
Bayezidtanmay <bayzidtonmoy123@gmail.com>
2025-02-24 10:40:12 +0200

    Week - 2 (Strings, Numbers, Conditions, Function)

---
+++ Week 2/Basic Function/basic_01.js
@@ -0,0 +1,19 @@
+/* Create a function named add that:
+Takes two parameters, a and b.
+Returns the sum of a and b.
+Call the function with the arguments 10 and 5.
+Print the result using console.log(). 
+
+Bonus: Call the function multiple times with different arguments and print all the results.*/
+
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(10, 5));
+console.log(add(1, 57));
+console.log(add(7, 9));
+console.log(add(66, 0.876));
+console.log(add(16, 5.567));
+console.log(add(12, 5));
+console.log(add(23, 5));
+console.log(add(10, 20));
